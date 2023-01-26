b92d47cd21876d52916b8b92d2015ebf46658772
N152-U <79331748+N152-U@users.noreply.github.com>
2023-01-26 16:31:58 -0600

    feat: HomeController

---
+++ src/main/java/com/ansm/ejercicioSOMA/EjercicioSomaApplication.java
@@ -0,0 +1,13 @@
+package com.ansm.ejercicioSOMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioSomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioSomaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ansm/ejercicioSOMA/HomeController.java
@@ -0,0 +1,16 @@
+package com.ansm.ejercicioSOMA;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		System.out.println("Ejecutando HomeController");
+		return "index";
+	}
+}
