ec71560a44c83b30f24cfcd35e4f023b23cccf82
N152-U <79331748+N152-U@users.noreply.github.com>
2023-01-26 19:52:22 -0600

    feat: Daos

--- src/main/java/com/ansm/ejercicioSOMA/servicios/ConsignatarioServicio.java
+++ src/main/java/com/ansm/ejercicioSOMA/servicios/ConsignatarioServicio.java
@@ -2,10 +2,13 @@ package com.ansm.ejercicioSOMA.servicios;
 
 import java.util.List;
 
-
-
-
 public class ConsignatarioServicio {
-	 
-   
+	  /* public List<Consignatario> listarConsignatarios();
+    
+    public void guardar(Consignatario consignatario);
+    
+    public void eliminar(Consignatario consignatario);
+    
+    public Consignatario encontrarConsignatario(Consignatario consignatario);*/
+
 }
