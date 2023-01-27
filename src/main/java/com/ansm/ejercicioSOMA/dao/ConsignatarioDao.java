0bdaeb8bdfb4644782c3831540744f37cd1a85e4
N152-U <79331748+N152-U@users.noreply.github.com>
2023-01-27 10:13:59 -0600

    feat: Domain

---
+++ src/main/java/com/ansm/ejercicioSOMA/dao/ConsignatarioDao.java
@@ -0,0 +1,9 @@
+package com.ansm.ejercicioSOMA.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ansm.ejercicioSOMA.domain.Consignatario;
+
+public interface ConsignatarioDao extends JpaRepository<Consignatario, Long>{
+
+}
