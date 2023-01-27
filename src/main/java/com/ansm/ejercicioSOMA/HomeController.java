7332db89220351b63b1b6d4fa4cc45f7d2cebd68
N152-U <79331748+N152-U@users.noreply.github.com>
2023-01-26 18:31:45 -0600

    feat: RequestMapping

--- src/main/java/com/ansm/ejercicioSOMA/HomeController.java
+++ src/main/java/com/ansm/ejercicioSOMA/HomeController.java
@@ -4,21 +4,39 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ansm.ejercicioSOMA.domain.Cliente;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class HomeController {
 	
-	@Value("${index.saludo}")
-	private String saludo;
+
 
 	@GetMapping("/")
 	public String home(Model model) {
-		System.out.println("Ejecutando HomeController");
+		
+		Cliente cliente = new Cliente();
+		cliente.setClientenombre("Naraly");
+		
 		String mensaje="Hello worldfss";
 		model.addAttribute("mensaje",mensaje);
-		model.addAttribute("saludo",saludo);
+		model.addAttribute("cliente",cliente);
 		return "index";
 	}
+	
+
+	@RequestMapping(value = "/guardarCliente",method=RequestMethod.POST) 
+	@ResponseBody 
+	 public String guardarCliente(@RequestBody Cliente cliente){ 
+		return "Hola"; 
+	}
+	
+	
+
 }
